26dcf284a1059fa50341d7239c9c3ba44a7158f9
Sumit-SDE <2201030400062@silveroakuni.ac.in>
2024-10-06 17:24:23 +0530

    Added code

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require("express");
 const app = express();
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use(cors());
+
+app.post("/", async (req, res) => {
+    console.log("req", req);
+    return res.status(200).json({ success: true, message: "Server is running bro" });
+});
 
 app.listen(process.env.PORT, () => {
     console.log("App is running on port", process.env.PORT);
